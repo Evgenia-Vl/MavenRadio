2ac1b5b6635d25c8f71d0d030842bf21ca122650
EvgeniaVL <e9320915@yandex.ru>
2023-04-10 18:23:52 +0300

    Change2 src/

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -9,23 +9,38 @@ public class Radio {
     private int nowNumber = minNumber;
     private int nowVolume = minVolume;
 
-    public int getQuantity(){ return quantity;}
-    public int getNowNumber() { return nowNumber; }
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getNowNumber() {
+        return nowNumber;
+    }
+
     public int getNowVolume() {
         return nowVolume;
     }
-    public void setNowNumber(int newNowNumber) { nowNumber = newNowNumber; }
-    public void setNowVolume(int newNowVolume) { nowVolume = newNowVolume; }
+
+    public void setNowNumber(int newNowNumber) {
+        nowNumber = newNowNumber;
+    }
+
+    public void setNowVolume(int newNowVolume) {
+        nowVolume = newNowVolume;
+    }
 
     public void setToMaxNumber() {
         nowNumber = maxNumber;
     }
+
     public void setToMaxVolume() {
         nowVolume = maxVolume;
     }
+
     public void setToMinNumber() {
         nowNumber = minNumber;
     }
+
     public void setToMinVolume() {
         nowVolume = minVolume;
     }
@@ -40,45 +55,69 @@ public class Radio {
 
 
     public void setAcceptableNumber(int newNowNumber) {
-        if (newNowNumber < minNumber) { return; }
-        if (newNowNumber > maxNumber) { return; }
+        if (newNowNumber < minNumber) {
+            return;
+        }
+        if (newNowNumber > maxNumber) {
+            return;
+        }
         nowNumber = newNowNumber;
 
-        if (newNowNumber == maxNumber) { nowNumber = newNowNumber; }
-        if (newNowNumber < maxNumber) { nowNumber = newNowNumber; }
+        if (newNowNumber == maxNumber) {
+            nowNumber = newNowNumber;
+        }
+        if (newNowNumber < maxNumber) {
+            nowNumber = newNowNumber;
+        }
     }
 
     public void setAcceptableVolume(int newNowVolume) {
-        if (newNowVolume < minVolume) { return; }
-        if (newNowVolume > maxVolume) { return; }
+        if (newNowVolume < minVolume) {
+            return;
+        }
+        if (newNowVolume > maxVolume) {
+            return;
+        }
         nowVolume = newNowVolume;
 
-        if (newNowVolume == maxVolume) { nowVolume = newNowVolume; }
-        if (newNowVolume < maxVolume) { nowVolume = newNowVolume; }
+        if (newNowVolume == maxVolume) {
+            nowVolume = newNowVolume;
+        }
+        if (newNowVolume < maxVolume) {
+            nowVolume = newNowVolume;
+        }
     }
 
     public void prevNumber() {
-        if (nowNumber == minNumber) { nowNumber = maxNumber;
+        if (nowNumber == minNumber) {
+            nowNumber = maxNumber;
         } else {
-            nowNumber = nowNumber - 1; }
+            nowNumber = nowNumber - 1;
+        }
     }
 
     public void nextNumber() {
-        if (nowNumber == maxNumber) { nowNumber = minNumber;
+        if (nowNumber == maxNumber) {
+            nowNumber = minNumber;
         } else {
-            nowNumber = nowNumber + 1; }
+            nowNumber = nowNumber + 1;
+        }
     }
 
     public void minVolume() {
-        if (nowVolume == minVolume) { nowVolume = minVolume;
+        if (nowVolume == minVolume) {
+            nowVolume = minVolume;
         } else {
-            nowVolume = nowVolume - 1; }
+            nowVolume = nowVolume - 1;
+        }
     }
 
     public void maxVolume() {
-        if (nowVolume == maxVolume) { nowVolume = maxVolume;
+        if (nowVolume == maxVolume) {
+            nowVolume = maxVolume;
         } else {
-            nowVolume = nowVolume + 1; }
+            nowVolume = nowVolume + 1;
+        }
     }
 }
 
